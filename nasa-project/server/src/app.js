e51f31ebdeda0c60ba6dc372039342dda116dc5b
Oleg Zazerin <oleg.zazerin@gmail.com>
2023-12-27 01:08:42 +0200

    [NASA PROJECT] Add root package.json.

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const path =require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+const CLIENT_SPA_APP_PATH = path.join(__dirname, '..', 'public');
+
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+// }));
 app.use(express.json());
+app.use(express.static(CLIENT_SPA_APP_PATH));
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(CLIENT_SPA_APP_PATH, 'index.html'));
+})
 
 app.use('/planets', planetsRouter);
 
